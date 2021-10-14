3d57be1827c3a653cce6fdc36772721c12e54fab
Artificial Thinker <67840255+Artificial-dev@users.noreply.github.com>
2021-10-14 23:03:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 class Config(object):
-	BOT_TOKEN = os.environ.get('BOT_TOKEN')
-	USERNAME = os.environ.get('USERNAME')
-	PASSWORD = os.environ.get('PASSWORD')
+	BOT_TOKEN = os.environ.get('BOT_TOKEN', '2093268638:AAGud7OsJE-zE5Z3_jMoKV80FRXfQthPmzg')
+	USERNAME = os.environ.get('USERNAME', 'pawarabhishek9822@gmail.com')
+	PASSWORD = os.environ.get('PASSWORD', '/)@uf&NHHGU2V7)')
